e495a5bc5bb4c622a89a6bfef72d6716ba745911
Game Android 2017 HD <musaalmasih@gmail.com>
2021-03-05 01:26:03 +0700

    Update ganas.js

--- ganas.js
+++ ganas.js
@@ -129,7 +129,7 @@ j++;
 }
 if (a_to_vh == false) {
 var encryptedUrl = Base64.encode(a_to_vi);
-a_to_ve[i].href = "https://kaguyanime.com/anti-ribet-daftar-aplikasi-pinjaman-online-tanpa-slip-gaji/?url=" + encryptedUrl;
+a_to_ve[i].href = "https://nyantaidulu.com/linknya/?url=" + encryptedUrl;
 a_to_ve[i].rel = "nofollow";
 a_to_vb++;
 a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
